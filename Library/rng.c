77fc8779b8c09b2dc2c4f6f38598f2a796f7e323
pau.clusella <s02pc5@>
2018-06-28 13:16:07 +0200

    Minimal version - no GSL

--- Library/rng.c
+++ Library/rng.c
@@ -3,12 +3,18 @@
         #include "rng.h"
 #endif
 
+// TO USE GSL RNG
+//void initialize_rng(int seed){
+//        gsl_rng_env_setup();
+//
+//        T = gsl_rng_default;
+//        rrr = gsl_rng_alloc(T);
+//
+//	gsl_rng_set(rrr,seed);
+//	return ;
+//}
+
 void initialize_rng(int seed){
-        gsl_rng_env_setup();
-
-        T = gsl_rng_default;
-        rrr = gsl_rng_alloc(T);
-
-	gsl_rng_set(rrr,seed);
+	srand(seed);
 	return ;
 }
--- Library/rng.h
+++ Library/rng.h
@@ -1,15 +1,32 @@
-#ifndef GSL_H
-#define GSL_H
-        #include <gsl/gsl_rng.h>
+#ifndef STD_H
+#define STD_H
+        #include <stdio.h>
+        #include <stdlib.h>
+        #include <time.h>
+        #include <sys/time.h>
+        #include <string.h>
+        #include <math.h>
+        #include <errno.h>
+        #include "macros.h"
 #endif
 
 
-#define RAND (gsl_rng_uniform(rrr)) 		// random double beween 0 and 1
-#define RANDIFF (-1+2.*RAND) 			// random double between -1 and 1
-#define IRAND(n) (gsl_rng_uniform_int(rrr,n)) 	// random integer between 0 and n-1 (inclusive)
+// GSL is not used by default
+//#ifndef GSL_H
+//#define GSL_H
+//        #include <gsl/gsl_rng.h>
+//#endif
 
-const gsl_rng_type * T;
-gsl_rng * rrr;
+//#define RAND (gsl_rng_uniform(rrr)) 			// random double beween 0 and 1
+//#define RANDIFF (-1+2.*RAND) 				// random double between -1 and 1
+//#define IRAND(n) (gsl_rng_uniform_int(rrr,n)) 	// random integer between 0 and n-1 (inclusive)
+
+#define RAND (((double)rand())/RAND_MAX) 	// random double beween 0 and 1
+#define IRAND(n) ((int) n*RAND) 		// random double between -1 and 1
+#define RANDIFF (-1+2.*RAND)			// random integer between 0 and n-1 (inclusive)
+
+//const gsl_rng_type * T;
+//gsl_rng * rrr;
 
 
 void initialize_rng(int seed);
